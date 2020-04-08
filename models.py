c46be6fa5d2a90a1a0491b4b61a8f5858a4e83d4
ckoerber <software@ckoerber.com>
2020-04-08 17:30:33 +0200

    Added first model class

---
+++ models.py
@@ -0,0 +1,209 @@
+"""Models to analyze COVID-19 data
+"""
+from typing import Generator, Tuple, TypeVar, Callable, Optional, Dict
+
+from numpy import array, exp
+
+FloatLike = TypeVar("FloatLike")
+FloatLikeArray = TypeVar("FloatLikeArray")
+
+
+def sir_step(
+    susceptible: FloatLike,
+    infected: FloatLike,
+    hospitalized: FloatLike,
+    recovered: FloatLike,
+    **kwargs
+) -> Tuple[FloatLike, FloatLike, FloatLike, FloatLike]:
+    """Executes SIR step and patches results such that each component is larger zero.
+
+    Arguments:
+        susceptible: Susceptible population
+        infected: Infected population
+        hospitalized: Hospitalized population
+        recovered: Recovered population
+        kwargs: FloatLike
+            beta_i: Growth rate for infected
+            gamma_i: Recovery rate for infected
+            hospitalization_rate: Percent of new cases becoming hospitalized
+
+    Returns:
+        S, I, H, R: FloatLike
+            Hospitalized is computed by growth from
+            `(r(t) + i(t) - r(t-1) - i(t-1)) * hospitalization_rate`
+    """
+    ever_infected_before = infected + recovered
+    total = susceptible + ever_infected_before
+
+    is_grow = kwargs["beta_i"] * susceptible * infected
+    ir_loss = kwargs["gamma_i"] * infected
+
+    susceptible -= is_grow
+    infected += is_grow - ir_loss
+    recovered += ir_loss
+
+    susceptible = max(susceptible, 0)
+    infected = max(infected, 0)
+    recovered = max(recovered, 0)
+
+    rescale = total / (susceptible + infected + recovered)
+
+    susceptible *= rescale
+    infected *= rescale
+    recovered *= rescale
+
+    ever_infected = infected + recovered
+
+    hospitalized = ever_infected - ever_infected_before
+    hospitalized *= kwargs.get("hospitalization_rate", 1)
+
+    return (
+        susceptible,
+        infected,
+        hospitalized,
+        recovered,
+    )
+
+
+def sihr_step(  # pylint: disable=R0913
+    susceptible: FloatLike,
+    infected: FloatLike,
+    hospitalized: FloatLike,
+    recovered: FloatLike,
+    **kwargs
+) -> Tuple[FloatLike, FloatLike, FloatLike, FloatLike]:
+    """Executes SIHR step and patches results such that each component is larger zero.
+
+    Arguments:
+        susceptible: Susceptible population
+        infected: Infected population
+        hospitalized: Hospitalized population
+        recovered: Recovered population
+        kwargs: FloatLike
+            beta_i: Growth rate for infected
+            gamma_i: Recovery rate for infected
+            beta_h: Growth rate for hospitalized
+            gamma_i: Recovery rate for hospitalized
+            capacity: Maximal number of hospitalized population
+
+    Returns:
+        S, I, H, R: FloatLike
+    """
+    total = susceptible + infected + recovered + hospitalized
+
+    is_grow = kwargs["beta_i"] * susceptible * infected
+    ir_loss = kwargs["gamma_i"] * infected
+    ih_loss = min(
+        kwargs["beta_h"] * infected, max(kwargs["capacity"] - hospitalized, 0)
+    )
+    hr_loss = kwargs["gamma_h"] * hospitalized
+
+    susceptible -= is_grow
+    infected += is_grow - ih_loss - ir_loss
+    hospitalized += ih_loss - hr_loss
+    recovered += ir_loss + hr_loss
+
+    susceptible = max(susceptible, 0)
+    infected = max(infected, 0)
+    hospitalized = max(hospitalized, 0)
+    recovered = max(recovered, 0)
+
+    rescale = total / (susceptible + infected + recovered + hospitalized)
+
+    return (
+        susceptible * rescale,
+        infected * rescale,
+        hospitalized * rescale,
+        recovered * rescale,
+    )
+
+
+def model_iterator(
+    n_days: int,
+    sir_fcn: Callable,
+    *args,
+    beta_i_fcn: Optional[Callable] = None,
+    **kwargs
+) -> Generator[Tuple[FloatLike, FloatLike, FloatLike, FloatLike]]:
+    """Iterates model to build up SIR data
+
+    Initial data is at day zero (no step).
+
+    Arguments:
+        n_days: Number of days to iterate
+        sir_fcn: The SIR model step function
+        beta_i_fcn: Function which maps infected growth for given kwargs
+        kwargs: Parameters to consturct beta_i schedule and sir step
+    """
+    pars = kwargs.copy()
+    beta_i_schedule = (
+        beta_i_fcn(n_days, **kwargs)
+        if beta_i_fcn is not None
+        else [kwargs.get("beta_i", None)] * n_days
+    )
+
+    for beta_i in enumerate(beta_i_schedule):
+        yield args
+        pars["beta_i"] = beta_i
+        args = sir_fcn(*args, **pars)
+
+
+def one_minus_logistic_fcn(  # pylint: disable=C0103
+    x: FloatLikeArray,
+    amplitude: FloatLike = 1.0,
+    decay_width: FloatLike = 1.0,
+    x0: FloatLike = 0.0,
+) -> FloatLikeArray:
+    """Computes `1 - A / (1 + exp(-w(x-x0)))`.
+    """
+    return 1 - amplitude / (1 + exp(-decay_width * (x - x0)))
+
+
+class FitFcn:  # pylint: disable=R0903
+    """Fit function wrapper
+    """
+
+    _columns = ("susceptible", "infected", "hospitalized", "recovered")
+
+    def __init__(
+        self,
+        sir_fcn: Callable,
+        beta_i_fcn: Optional[Callable],
+        columns: Tuple[str] = ("infected", "hospitalized"),
+    ):
+        """
+        """
+        self.col_index = []
+        for col in columns:
+            if not col in self._columns:
+                raise KeyError(
+                    "Key {col} not in expected columns {cols}".format(
+                        col=col, cols=self._columns
+                    )
+                )
+            for i, icol in enumerate(self._columns):
+                if col == icol:
+                    self.col_index.append(i)
+                    break
+
+        self.columns = columns
+        self.sir_fcn = sir_fcn
+        self.beta_i_fcn = beta_i_fcn
+
+    def __call__(self, n_days: int, p: Dict[str, FloatLike]) -> FloatLikeArray:
+        """
+        """
+        kwargs = p.copy()
+        args = (
+            kwargs.pop("initial_susceptible"),
+            kwargs.pop("initial_infected"),
+            kwargs.pop("initial_hospitalized"),
+            kwargs.pop("initial_recovered"),
+        )
+        return array(
+            list(
+                model_iterator(
+                    n_days, self.sir_fcn, *args, beta_i_fcn=self.beta_i_fcn, **kwargs
+                )
+            )
+        )[self.col_index]
