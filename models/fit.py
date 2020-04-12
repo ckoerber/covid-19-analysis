81f9b4df5df9b30ee49713c5cb33e9ccaf573c8a
ckoerber <software@ckoerber.com>
2020-04-12 15:22:15 +0200

    Added type casting to fit func

--- models/fit.py
+++ models/fit.py
@@ -2,9 +2,10 @@
 """
 from typing import Dict, Optional, List, Callable, Union
 
+from numpy import log
 from pandas import DataFrame
 
-from models.utils import FloatLike, FloatLikeArray, model_iterator
+from models.utils import FloatLike, FloatLikeArray, model_iterator, get_doubling_time
 
 
 class FitFcn:  # pylint: disable=R0903
@@ -24,7 +25,9 @@ class FitFcn:  # pylint: disable=R0903
         Arguments:
             sir_fcn: The function which executes a SIR step
             beta_i_fcn: Function which generates a schedule for the growth rate
-            columns: Function call return specified columns as arrays
+            columns: Function call return specified columns only
+            as_array: If true, function call returns array. Else DataFrame.
+            drop_rows: Drop selected rows from function call.
         """
         self.sir_fcn = sir_fcn
         self.beta_i_fcn = beta_i_fcn
@@ -54,6 +57,18 @@ class FitFcn:  # pylint: disable=R0903
 
         kwargs = dict(p)
         kwargs["capacity"] = x.get("capacity", None)
+
+        recovery_days = kwargs.pop("recovery_days", None)
+        if recovery_days is not None:
+            kwargs["gamma_i"] = 1 / (recovery_days / x["bin_size"])
+            kwargs["gamma_h"] = 1 / (recovery_days / x["bin_size"])
+
+        inital_doubling_time = kwargs.pop("inital_doubling_time", None)
+        if inital_doubling_time is not None:
+            kwargs["beta_i"] = (
+                log(2) / (inital_doubling_time / x["bin_size"]) + kwargs["gamma_i"]
+            ) / s
+
         data = {
             "susceptible": s if s is not None else kwargs.pop("initial_susceptible"),
             "infected": i if i is not None else kwargs.pop("initial_infected"),
